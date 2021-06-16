efe314cedaac1db30bdb9e349652401ac26254f4
Wan Muzaffar <wanmuz86@gmail.com>
2021-06-16 15:04:15 +0800

    add item

--- src/App.vue
+++ src/App.vue
@@ -35,7 +35,7 @@
     form-control mb-3" v-model="newTodo.assignee"/>
     <button class="btn btn-primary" v-on:click="addItem">Add</button>
     </div>
-    <div class="card p-5 bg-light mb-3">
+    <div class="card p-5 bg-light mb-3" v-if="todos.length > 0 ">
       <h3>The WODs</h3>
       <table class="table table-bordered table-striped table-hover">
         <thead>
@@ -47,7 +47,8 @@
     
         </thead>
         <tbody>
-          <tr v-for="todo in todos" :key="todo.name">
+          <tr v-for="todo in todos" :key="todo.name"
+           v-on:click="rowClicked(todo)">
             <td>{{todo.name}}</td>
             <td>{{todo.difficulty}}</td>
             <td>{{todo.assignee}}</td>
@@ -55,19 +56,22 @@
         </tbody>
       </table>
       </div> 
-      <div class="card mb-3 bg-light p-5">
+      <div class="card mb-3 bg-light p-5" v-if="selectedData">
       <h3>Detail WOD</h3>
-      <p>WOD name</p>
-      <p>WOD difficulty</p>
-      <p>WOD assignee</p>
+      <p>{{selectedData.name}}</p>
+      <p>{{selectedData.difficulty}}</p>
+      <p>{{selectedData.assignee}}</p>
+      <p v-if="selectedData.completed">Completed</p>
+      <p v-if="!selectedData.completed">Not Completed</p>
       <div class="row">
       <div class="col-6">
       <button class="btn btn-success" 
-      style="width:100%">Mark as Done</button>
+      style="width:100%" v-on:click="markAsDonePressed">
+      Mark as Done</button>
       </div>
       <div class="col-6">
       <button class="btn btn-danger" 
-      style="width:100%" >Delete</button>
+      style="width:100%" v-on:click="deletePressed">Delete</button>
       </div> 
       </div>
       </div> 
@@ -94,10 +98,12 @@ export default {
       hasError:false,
       username:"",
       todos:[],
+      selectedData:null,
       newTodo:{
         "name":'',
         "difficulty":0,
-        "assignee":''
+        "assignee":'',
+        "completed":false
       },
       difficultyOptions:[
           "Easy",
@@ -128,18 +134,51 @@ export default {
     },
     addItem:function(){
       // TODO do verification first
-      if (this.newTodo.name != "" && this.newTodo.difficulty == 0){
+      if (this.newTodo.name != "" && this.newTodo.difficulty != 0){
       this.todos.push(this.newTodo);
+      /// Saving data using localStorage
+      localStorage.setItem("todos",JSON.stringify(this.todos));
       this.newTodo = {
         "name":'',
         "difficulty":0,
-        "assignee":''
+        "assignee":'',
+        "completed":false
       }
       }
       else {
         alert("Name cannot empty");
       }
+    },
+    rowClicked:function(data){
+      console.log(data);
+      this.selectedData = data
+    },
+    deletePressed:function(){
+
+      ///Declarative Programming... (map, filter, reduce)
+      // filter here 
+      // return everything that fill this condition
+      // val != this.selectedData;
+      let newTodos = this.todos.filter(val=>{
+        return val != this.selectedData;
+      })
+      this.todos = newTodos
+      this.selectedData = null
+      localStorage.setItem("todos",JSON.stringify(this.todos));
+   
+    },
+    markAsDonePressed:function(){
+      this.selectedData.completed = true;
+      localStorage.setItem("todos",JSON.stringify(this.todos));
     }
+  },
+  mounted:function(){
+    // all your initialization will happen here...
+    let retrieveData = localStorage.getItem('todos');
+    if (retrieveData){
+      this.todos = JSON.parse(retrieveData);
+    }
+
   }
 }
 </script>
